06f8b0782ba236b4d4c13f9dbce6e7c12830ab0b
edezuraud <edezuraud@gedicom.fr>
2022-07-03 21:46:39 +0200

    add hex et rgb colors

--- index.js
+++ index.js
@@ -4,16 +4,46 @@ const port = process.env.PORT || 5566
 
 const getColors = require('get-image-colors')
 
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 
 app.get('/api', async (req, res) => {
 
     try {
         const url = req.query.url;
         if (url) {
-            colors = await getColors(url);
+            response = await getColors(url);
+            const json = await response.json();
+
+            const child = { rgb: '', hex: '' };
+
+            const colors = {
+                primary: child,
+                secondary: child,
+                tertiary: child
+            };
+
+            for (var i = 0; i < json.length; i++) {
+                if (i < 3) {
+                    const key = Object.keys(ret)[i];
+                    const rgbColor = JSON.stringify(json[i]._rgb);
+                    colors[key].rbg = rgbColor.replace('[', '').replace(']', '');
+
+                    const hexColor = rgbToHex(json[i]._rgb[0], json[i]._rgb[1], json[i]._rgb[2]);
+                    colors[key].hex = hexColor;
+                }
+            }
+
             res.send(colors);
         } else {
-            res.send('No url, try to use : xxx?url=yourImageUrl');
+            res.send('No url, try to use : ?url=yourImageUrl');
         }
     } catch (error) {
         throw error;
